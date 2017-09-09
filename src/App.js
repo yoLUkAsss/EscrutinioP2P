a04d05ba831452e540a48615ded63c3a39d2f66b
jesus <laimejesu@gmail.com>
2017-09-09 19:25:33 -0300

    added Election component, added instantiate Election contract button, added show address of deployed contract

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import ElectionContract from '../build/contracts/Election.json'
+import Election from './Election.js'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -6,28 +8,46 @@ import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
 
+//useful functions
+//console.log(JSON.stringify(json, undefined, 2))
+//this.state.web3.toAscii(x)
+
 class App extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
-      storageValue: 0,
+      depAddress: "",
+      electionName: "",
       web3: null
     }
   }
-
   componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
-
-    getWeb3
-    .then(results => {
+    getWeb3.then(results => {
       this.setState({
         web3: results.web3
       })
-    })
-    .catch(() => {
+    }).catch(() => {
       console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract(event) {
+    event.preventDefault()
+    const name = event.target.election.value
+    const contract = require('truffle-contract')
+    const election = contract(ElectionContract)
+    var electionInstance
+    election.setProvider(this.state.web3.currentProvider)
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      election.deployed().then((instance) => {
+        console.log(JSON.stringify(instance, undefined, 2))
+        electionInstance = instance
+        return electionInstance.setName(name, {from: accounts[0]})
+      }).then((setResult) => {
+        return electionInstance.getName.call(accounts[0])
+      }).then((getResult) => {
+        return this.setState({depAddress: electionInstance.address, electionName: getResult})
+      })
     })
   }
 
@@ -35,18 +55,20 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Nothing yet</a>
+            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
         </nav>
 
         <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Bienvenido a DEscrutinio!</h1>
-              <p>La aplicación se encuentre en sus primeras instancias de desarrollo.</p>
-              <p>Con lo cual, aún no es posible utilizar ninguna de los servicios a prestar </p>
-              <p>Ante cualquier duda contactarse con <strong>sandoval.lucasj@gmail.com</strong> o <strong>laimejesu@gmail.com</strong></p>
-            </div>
+
+          <div>
+            <Election submitName={this.instantiateContract.bind(this)}/>
+            <p>
+              Address dep:<br/>
+              {this.state.depAddress}
+            </p>
           </div>
+
+
         </main>
       </div>
     );
