d88f51843d93c70b3a74c9c12af12edbc1778d84
jesus <laimejesu@gmail.com>
2017-09-09 19:26:25 -0300

    added deploy Election.sol in migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,14 @@
 //var Contract = artifacts.require("./path/to/Contract.sol");
-
+var Election = artifacts.require("./Election.sol");
 module.exports = function(deployer) {
   //deployer.deploy(SimpleStorage);
 
   /**
-   * Example of use : 
-   * 
+   * Example of use :
+   *
    * deployer.deploy(Contract);
-   * 
+   *
    */
+   deployer.deploy(Election);
 
 };
