d0a5fa4dab6386e11c12cad18bd701f382022452
jesus <laimejesu@gmail.com>
2017-09-09 19:23:53 -0300

    added Election.js react component

---
+++ src/Election.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Election extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: 'Not election yet',
+            message : ''
+        };
+        this.showName = this.showName.bind(this);
+    }
+
+    showName(event) {
+        event.preventDefault();
+        this.setState({
+            name : event.target.value,
+            message: 'Preselected name: ' + event.target.value
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <h2> Create New Election </h2>
+                <h4>
+                    {this.state.message}
+                </h4>
+                <form onChange={this.showName} onSubmit={this.props.submitName}>
+                    <input
+                        type="text"
+                        placeholder="Name for election"
+                        name="election"/>
+                    <input type="submit" value="Guardar" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Election
