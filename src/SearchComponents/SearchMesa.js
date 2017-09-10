ddce1097bba014bc7f78cdfb43e9d3a74517fea1
pcm-exilio <sandoval.lucasj@gmail.com>
2017-09-10 15:52:15 -0300

    Added - 404html page, added default routing, initial search mesa html

---
+++ src/SearchComponents/SearchMesa.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import Center from 'react-center'
+
+class SearchMesa extends Component {
+
+    render() {
+        return (
+            <Center>
+            <div>
+            
+                <h4> Will be covered </h4>
+
+                <h1> Busqueda de mesa </h1>
+
+                <div id="searchMesa" > </div>
+
+            </div>
+            </Center>
+        )
+    }
+}
+
+export default SearchMesa
--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import App from './App.js'
 import About from './About.js'
 import Home from './Home.js'
 
+import Error404 from './ErrorComponents/Error404.js'
+
+import SearchMesa from './SearchComponents/SearchMesa.js'
+
+import './ErrorComponents/Error404.css'
+
 ReactDOM.render(
   <Router>
-    <div>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/app" component={App}/>
-      <Route exact path="/about" component={About}/>
-    </div>
+      <div>
+        <Switch>
+
+          {/* Paths for Router React */}
+          <Route exact path="/" component={Home}/>
+          
+          <Route exact path="/app" component={App}/>
+          <Route exact path="/mesa" component={SearchMesa}/> 
+          <Route exact path="/about" component={About}/>
+          
+
+
+
+          {/* Final matches every other path - renders Error 404 not found */}
+          <Route component={Error404} />
+        </Switch>
+      </div>
   </Router>,
   document.getElementById('root')
 );
