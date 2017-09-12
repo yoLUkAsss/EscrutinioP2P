164448bcbbe2ac9073f15df6ec34789d5373aba7
pcm-exilio <sandoval.lucasj@gmail.com>
2017-09-12 18:39:57 -0300

    Try number 3

--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
-    development: {
-      host: "localhost",
+    dev: {
+      host: "190.220.180.34",
       port: 8545,
       network_id: "*" // Match any network id
     }
