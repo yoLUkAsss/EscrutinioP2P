9481397e27eac4ded0819a02cd7c950e7df61944
jesus <laimejesu@gmail.com>
2017-09-21 14:43:34 -0300

    added UsuarioCRUD in deployed contracts for testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 //var Contract = artifacts.require("./path/to/Contract.sol");
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var Election = artifacts.require("./Election.sol");
+var UserCRUD = artifacts.require("./UsuarioCRUD.sol");
 // var Mesa = artifacts.require("./Mesa.sol");
 // var Mesa = artifacts.require("./Mesa.sol");
 module.exports = function(deployer) {
@@ -13,6 +14,7 @@ module.exports = function(deployer) {
    *
    */
    deployer.deploy(Election);
+   deployer.deploy(UserCRUD);
   //  deployer.deploy(Mesa);
   //  deployer.deploy(Mesa);
 };
