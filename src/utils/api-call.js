8b9930f4bcc55c0dc9836ffff49dd325f63ac51b
jesus <laimejesu@gmail.com>
2017-11-01 14:38:44 -0300

    added some posts

--- src/utils/api-call.js
+++ src/utils/api-call.js
@@ -13,3 +13,28 @@ export function login(userEmail, userPassword){
 export function logout(userEmail){
   return axios.post(urlBase + 'users/logout', {email : userEmail})
 }
+
+export function initDistrito(userEmail, distrito, cantidadDeEscuelas){
+  return axios.post(urlBase + 'locations/initdistrito',
+  { email : userEmail ,
+    distritoId: distrito,
+    escuelas: cantidadDeEscuelas
+  })
+}
+
+export function initEscuela(userEmail, distrito, escuela, cantidadDeMesas){
+  return axios.post(urlBase + 'locations/initescuela',
+  { email : userEmail ,
+    distritoId: distrito,
+    escuelaId: escuela,
+    mesas : cantidadDeMesas
+  })
+}
+
+export function initElection(userEmail, newCandidates){
+  return axios.post(urlBase + 'election/initelection',
+  {
+    email : userEmail,
+    candidates : newCandidates
+  })
+}
