f67a0e4b39c5f56380a0ed620f51847ce7869407
jesus <laimejesu@gmail.com>
2017-11-17 09:49:22 -0300

    added results item, added clean election info in logout

--- src/NavBarComponents/LogOutItem.js
+++ src/NavBarComponents/LogOutItem.js
@@ -9,20 +9,9 @@ import AlertContainer from 'react-alert'
 /**
  * Components
  */
-
-/**
- * Controller for Component
- */
-// import getWeb3 from '../utils/getWeb3'
 import * as utils from '../utils/utils.js'
 import * as currentUser from '../utils/user_session.js'
 import * as api from '../utils/api-call.js'
-
-/**
-* Contracts
-*/
-// import UserContract from '../../build/contracts/User.json'
-// import contract from 'truffle-contract'
 
 /**
 usa los siguientes props:
@@ -34,6 +23,7 @@ class LogOutItem extends Component {
       event.preventDefault()
       api.logout(currentUser.getAddress(cookie)).then((res) => {
         currentUser.clean(cookie)
+        currentUser.cleanElection(cookie)
         utils.showSuccess(this.msg, "Cierre de sesion exitoso", () => {this.props.history.push("/")})
         // utils.showSuccess(this.msg, "Cierre de sesion exitoso")
       }).catch((err) => {
